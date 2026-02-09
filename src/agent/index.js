e4281b7a15cce75d6fdc7c290914232f0de4c6e6
Antonio Membrides Espinosa <tonykssa@gmail.com>
2026-02-09 16:04:16 +0100

    add main files

---
+++ src/agent/index.js
@@ -0,0 +1,13 @@
+import { ChatOllama } from "@langchain/ollama";
+
+try {
+    const model = new ChatOllama({
+        model: "phi3:mini",
+        baseUrl: "http://127.0.0.1:11434"
+    });
+
+    const res = await model.invoke("Hola desde LangChain JS");
+    console.log(res.content);
+} catch (error) {
+    console.error("Error al conectar con Ollama:", error);
+}
